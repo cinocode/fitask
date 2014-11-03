32366719278b67c591d07bc23eb69c4ccb6d3c55
Ole Hansen <ole.hansen@fortis-it.de>
2014-11-03 20:20:24 +0100

    Added create controller action

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -2,4 +2,11 @@ class TasksController < ApplicationController
   def index
     @tasks = Task.where(['completion_ts is null'])
   end
+
+  def create
+    @task = Task.new(:text => params[:new_task_text])
+    if @task.save
+      redirect_to tasks_path
+    end
+  end
 end
