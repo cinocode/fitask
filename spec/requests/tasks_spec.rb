a15877d003d6316b54d12408bb0b884e5680a34a
Ole Hansen <ole.hansen@fortis-it.de>
2014-11-03 19:34:33 +0100

    Adding new testcase to ensure completed tasks are not displayed

--- spec/requests/tasks_spec.rb
+++ spec/requests/tasks_spec.rb
@@ -3,7 +3,9 @@ require 'rails_helper'
 RSpec.describe "Tasks", :type => :request do
   describe "opening tasks index page" do
     before(:each) do
-      @task = Task.create(:text => 'Write Jive blogpost about Ruby on Rails')
+      @todo = Task.create(:text => 'Write Jive blogpost about Ruby on Rails')
+      @done = Task.create(:text => 'Drink wine',
+                         :completion_ts => Time.now)
     end
 
     it "should display headline" do
@@ -14,6 +16,11 @@ RSpec.describe "Tasks", :type => :request do
     it "should display tasks name" do
       visit tasks_path
       expect(page).to have_content('Write Jive blogpost about Ruby on Rails')
+    end
+
+    it "should not display completed tasks" do
+      visit tasks_path
+      expect(page).to_not have_content('Drink wine')
     end
   end
 end
